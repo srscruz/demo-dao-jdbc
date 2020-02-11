ec6dfe0f339bffb7b8420effca75376c44a3072a
sergio cruz <sergio.cruz@sages4it.com>
2020-02-11 16:35:23 -0300

    implementação da busca de vendedor por departamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,27 @@
 package application;
 
-
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		
+
 		SellerDao sellerDao = DaoFactory.createSallerDao();
 		Seller seller = sellerDao.findById(9);
 		System.out.println("=== Test 1 : FindById ===");
 		System.out.println(seller);
 
-	}
+		System.out.println("\n=== Test 2 : Seller FindByDepartment ===");
 
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+	}
 }
