dfec01dae5fe31b59cf3673d94b2be02439d45cc
sergio cruz <sergio.cruz@sages4it.com>
2020-02-11 14:58:07 -0300

    implemetado o metodo findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,22 +2,18 @@ package application;
 
 
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "TI");
-		Seller seller = new Seller(1021, "Bob", "bob@gmail.com", new Date(), 1000.0, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSallerDao();
-		System.out.println(obj);
+		Seller seller = sellerDao.findById(9);
+		
 		System.out.println(seller);
 
 	}
